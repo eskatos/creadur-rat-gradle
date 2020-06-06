5c1bcee8bc4f75f4d2bec28bd0429841c39473eb
Paul Merlin <paul@nosphere.org>
2020-06-06 12:48:51 +0200

    Test Gradle from 6.0 to 6.5

--- src/test/kotlin/org/nosphere/apache/rat/AbstractPluginTest.kt
+++ src/test/kotlin/org/nosphere/apache/rat/AbstractPluginTest.kt
@@ -43,6 +43,10 @@ abstract class AbstractPluginTest(
         fun testMatrix() = listOf(
             TestMatrix(GradleVersion.version("6.5"), true),
             TestMatrix(GradleVersion.version("6.5")),
+            TestMatrix(GradleVersion.version("6.4.1")),
+            TestMatrix(GradleVersion.version("6.3")),
+            TestMatrix(GradleVersion.version("6.2.2")),
+            TestMatrix(GradleVersion.version("6.1.1")),
             TestMatrix(GradleVersion.version("6.0"))
         )
     }
