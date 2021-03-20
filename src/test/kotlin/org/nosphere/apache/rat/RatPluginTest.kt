bcbe6bae23cc865c0e19dd87a00afeed8ba739d6
Paul Merlin <paul@nosphere.org>
2021-03-20 14:38:29 +0100

    Relax test assertions to work on Windows

--- src/test/kotlin/org/nosphere/apache/rat/RatPluginTest.kt
+++ src/test/kotlin/org/nosphere/apache/rat/RatPluginTest.kt
@@ -235,7 +235,7 @@ class RatPluginTest(testMatrix: TestMatrix) : AbstractPluginTest(testMatrix) {
     private
     fun BuildResult.assertOutputContainsAuditFailureMessage(unApprovedLicenses: Int = 1) {
         assertThat(output, containsString("Apache Rat audit failure - $unApprovedLicenses unapproved license"))
-        assertThat(output, containsString(htmlReportFile.absolutePath))
+        assertThat(output, containsString(htmlReportFile.relativeTo(rootDir).path))
     }
 
     private
