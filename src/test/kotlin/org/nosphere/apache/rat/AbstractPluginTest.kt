507481516b4bf8154cbf42ad2ceeb89b8b23a0e2
paul <paul@nosphere.org>
2022-09-21 14:08:05 +0200

    Test against Gradle 7.5.1

--- src/test/kotlin/org/nosphere/apache/rat/AbstractPluginTest.kt
+++ src/test/kotlin/org/nosphere/apache/rat/AbstractPluginTest.kt
@@ -41,7 +41,7 @@ abstract class AbstractPluginTest(
         @Parameterized.Parameters(name = "{0}")
         @JvmStatic
         fun testMatrix() = listOf(
-            TestMatrix(GradleVersion.version("7.4.2"), true),
+            TestMatrix(GradleVersion.version("7.5.1"), true),
             TestMatrix(GradleVersion.version("7.0"), true),
             TestMatrix(GradleVersion.version("6.9.2"), true),
             TestMatrix(GradleVersion.version("6.0"))
