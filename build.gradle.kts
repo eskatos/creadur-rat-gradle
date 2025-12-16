c819b3cfb8144721b1cfbd9471150593e88d80f2
Robert Houghton <robert.c.d.houghton@gmail.com>
2025-12-16 09:09:26 -0800

    Update Apache RAT dependency version to 0.17
    Hoping to get a fix for https://issues.apache.org/jira/browse/RAT-265, to use ANT/git style patterns in excludeFiles.

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.apache.rat:apache-rat:0.15")
+    compileOnly("org.apache.rat:apache-rat:0.17")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation(gradleTestKit())
--- src/main/kotlin/org/nosphere/apache/rat/RatTask.kt
+++ src/main/kotlin/org/nosphere/apache/rat/RatTask.kt
@@ -38,7 +38,7 @@ import org.gradle.kotlin.dsl.*
 import javax.inject.Inject
 
 private
-const val ratVersion = "0.15"
+const val ratVersion = "0.17"
 
 @CacheableTask
 open class RatTask private constructor(
